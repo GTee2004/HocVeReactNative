4f3eee1b40f6c781163ecb765dbc1df5c3710e41
GTee2004 <giathanh232004@gmail.com>
2025-03-25 16:09:16 +0700

    TextInput

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-
-  // const [name, setName] = useState<string>("Thành");
-  // const [test, setTest] = useState({
-  //   name: 'hoidanit',
-  //   age: 25,
-  // });
   
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
 
   // jsx
   return (
     <View style={styles.container}>
+      <View>
+      <Text style={{fontSize: 40, color: "blue"}}>Name: {name}</Text>
+      <TextInput 
+        multiline
+        autoCapitalize={"words"}
+        onChangeText={(value) => setName(value)}
+        style={{
+          borderColor: "green",
+          borderWidth: 1,
+          width: 200,
+          padding: 15
+        }}
+      />
+      </View>
+      <View>
+      <Text style={{fontSize: 40, color: "violet"}}>Age: {age}</Text>
+      <TextInput 
+        multiline
+        keyboardType='numeric'
+        maxLength={2}
+        onChangeText={(value) => setAge(+value)}
+        style={{
+          borderColor: "green",
+          borderWidth: 1,
+          width: 200,
+          padding: 15
+        }}
+      />
+      </View>
       <Text style={{fontSize: 30, color: "red"}}>
         count = {count}
       </Text>
