da633918a73537e6cd53b9d812b96515f30d3d4d
GTee2004 <giathanh232004@gmail.com>
2025-03-25 15:26:26 +0700

    Text, View

--- App.tsx
+++ App.tsx
@@ -2,10 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  // jsx
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.header}>ABC</Text>
+        <Text style={styles.parent}>ABC 
+          <Text style={styles.child}>Bla Bla</Text></Text>
+      </View>
+      <Text style={styles.hello1}>Hello World!</Text>
+      <Text>Hello 2</Text>
     </View>
   );
 }
@@ -17,4 +24,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1: {
+    color: "red",
+        fontSize: 30, 
+        borderWidth: 1, 
+        borderColor: "black",
+        padding: 10
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "600",
+    textAlign: "center"
+  },
+  parent: {
+    fontSize: 60,
+    color: "green",
+    textAlign: "center"
+  },
+  child: {
+    fontSize: 30,
+    color: "pink"
+  }
 });
