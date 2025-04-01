a7976a5c2559026afa674fcd41f2dee9410b12f1
GTee2004 <giathanh232004@gmail.com>
2025-04-01 19:01:07 +0700

    Passing Parameters To Routers

--- components/Review/Detail.tsx
+++ components/Review/Detail.tsx
@@ -1,19 +1,30 @@
 import { Button, StyleSheet, Text, View } from "react-native"
 import { OPENSANS_REGULAR } from "../../utils/const";
-import { NavigationProp, ParamListBase, useNavigation } from "@react-navigation/native";
+import { NavigationProp, ParamListBase, RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 
 const styles = StyleSheet.create({
     review: {
         fontSize: 30,
         fontFamily: OPENSANS_REGULAR
+    },
+    reviewText: {
+        fontSize: 20,
+        marginBottom: 15,
+        color: "blue",
+        fontFamily: OPENSANS_REGULAR
     }
+
 })
 const DetailScreen = () => {
-
+    const route: RouteProp<RootStackParamList, 'review-detail'> = useRoute();
     const navigation: NavigationProp<RootStackParamList> = useNavigation();
+
     return (
         <View>
-            <Text style={styles.review}> detail screen google</Text>
+            <Text style={styles.review}>Review Detail</Text>
+            <Text style={styles.reviewText}>ID: {route.params?.id}</Text>
+            <Text style={styles.reviewText}>App: {route.params?.title}</Text>
+            <Text style={styles.reviewText}>Star: {route.params?.star}</Text>
             <Button title="Go Home" 
             onPress={() => {navigation.navigate("home")}} />
         </View>
--- components/Review/Home.tsx
+++ components/Review/Home.tsx
@@ -1,15 +1,59 @@
-import { Button, Text, View } from "react-native"
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
+
+
+interface IReview {
+    id: number; 
+    title: string;
+    star: number;
+}
 const HomeScreen = (props: any) => {
-    const { navigation } = props;
-    console.log(">>> check props: ", props);
+    const navigation: NavigationProp<RootStackParamList> = useNavigation();
+    const [review, setReview] = useState<IReview[]>([
+        { id: 1, title: "Google", star: 5 },
+        { id: 2, title: "Facebook", star: 4 },
+        { id: 3, title: "Instagram", star: 3 },
+        { id: 4, title: "Twitter", star: 2 },
+        { id: 5, title: "LinkedIn", star: 1 },
+        { id: 6, title: "Youtube", star: 5 },
+        { id: 7, title: "Netflix", star: 4 },
+        { id: 8, title: "Spotify", star: 3 },
+        { id: 9, title: "Snapchat", star: 2 },
+        { id: 10, title: "TikTok", star: 1 }
+    ]);
     return (
         <View>
-            <Text style={{ fontSize: 40 }}>hoidanit Home screen</Text>
-            <Button title="View Detail"
-                onPress={() => navigation.navigate("review-detail")} />
+            <Text style={{ fontSize: 30 }}>Review List:</Text>
+            
+            <View>
+                <FlatList
+                    data={review}
+                    keyExtractor={(item) => item.id + ""}
+                    renderItem={({ item }) => {
+                        return (
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate("review-detail", item)}
+                            > 
+                            <View style={styles.reviewItem}>
+                                <Text>{item.title}</Text>
+                            </View>
+                        </TouchableOpacity>
+                       )
+                    }}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    reviewItem: {
+        padding: 15,
+        backgroundColor: "#ccc",
+        margin: 15
+    }
+})
 
 export default HomeScreen;
